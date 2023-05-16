275ba16a7c805106d1f2b92764d72a527a778363
viliussapauskas <vilius.sapauskas.1997@gmail.com>
2023-05-16 22:15:08 +0300

    Move solana declaration so separate file

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,7 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-interface Window {
-  solana: any; // Replace 'any' with the appropriate type for the 'solana' object
-}
